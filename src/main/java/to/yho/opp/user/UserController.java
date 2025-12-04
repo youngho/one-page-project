b445b0c36768123f2defa1a76b82c315743f2d1d
youngho <xfile0304@gmail.com>
2025-12-04 11:52:04 +0900

    import 정리

--- src/main/java/to/yho/opp/user/UserController.java
+++ src/main/java/to/yho/opp/user/UserController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 public class UserController {
     private final UserService service;
